5be907e1c732684fbef54aee1399c0776cddfb1e
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-11-30 10:36:41 +0100

    CC-33 : Created api with exemple request

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -2,8 +2,9 @@ import {NgModule} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
+import { CleaverCooksApi } from './services/cleaver-cooks-api';
 
-const uri = 'localhost:3000'; // <-- add the URL of the GraphQL server here
+const uri = CleaverCooksApi.BACKEND_API_URL;
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
---
+++ src/app/services/cleaver-cooks-api.ts
@@ -0,0 +1,29 @@
+import { ApolloQueryResult } from "@apollo/client";
+import { Apollo, gql } from "apollo-angular";
+
+export class CleaverCooksApi {
+    static readonly BACKEND_API_URL:string = 'localhost:3000';
+
+    public getAllIngredients(apollo : Apollo) : Promise<unknown> {
+        return new Promise((resolve, reject) => {
+            const query = gql`
+                query GetAllIngredients {
+                    getAllIngredients {
+                        quantity
+                        name
+                        id
+                    }
+                }
+            `;
+            apollo.watchQuery({
+                query: query
+            }).valueChanges.subscribe(({data, error}) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    }
+}
