4a471b69151876529586ca41473264df0650111c
eliott <eliott.jaquier@gmail.com>
2023-12-07 16:10:33 +0100

    Added CRUD for ingredients + getAllReceipts

--- src/app/services/cleaver-cooks-api.ts
+++ src/app/services/cleaver-cooks-api.ts
@@ -1,13 +1,15 @@
-import { Apollo, gql } from "apollo-angular";
+import {Apollo, gql} from "apollo-angular";
+import {Recipe} from "../models/recipe";
+import {Ingredient} from "../models/ingredient";
 
 export class CleaverCooksApi {
-    public constructor(private apollo : Apollo) {
-    }
+  public constructor(private apollo: Apollo) {
+  }
 
-    public getAllIngredients() : Promise<any[]> {
-        return new Promise((resolve, reject) => {
-            try {
-                const query = gql`
+  public getAllIngredients(): Promise<Ingredient[]> {
+    return new Promise((resolve, reject) => {
+      try {
+        const query = gql`
                     query GetAllIngredients {
                         getAllIngredients {
                             name
@@ -15,37 +17,101 @@ export class CleaverCooksApi {
                         }
                     }
                 `;
-                this.apollo.watchQuery({
-                    query: query
-                }).valueChanges.subscribe(({data, error}) => {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve((data as any).getAllIngredients);
-                    }
-                });
-            } catch (error) {
-                reject(error);
-            }
+        this.apollo.watchQuery({
+          query: query
+        }).valueChanges.subscribe(({data, error}) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve((data as any).getAllIngredients);
+          }
         });
-    }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-    public addIngredient(name:String){
+  public addIngredient(name: String) {
+    return new Promise((resolve, reject) => {
       const query = gql`
-            mutation CreateIngredient($name: String!) {
-              createIngredient(name: $name) {
-                id
-                name
+              mutation CreateIngredient($name: String!) {
+                createIngredient(name: $name) {
+                  id
+                }
               }
-            }
-        `;
-      this.apollo.mutate({mutation:query, variables:{ name:name }}) .subscribe(
-        ({ data }) => {
-          console.log('got data', data);
-        },
-        error => {
-          console.log('there was an error sending the query', error);
-        },
-      );
-    }
+          `;
+      this.apollo.mutate({mutation: query, variables: {name: name}}).subscribe(result => {
+        if (result.errors) {
+          reject(result.errors);
+        } else {
+          resolve(result.data);
+        }
+      });
+    });
+  }
+
+  public deleteIngredient(id: String) {
+    return new Promise((resolve, reject) => {
+      const query = gql`
+              mutation DeleteIngredient($id: String!) {
+                deleteIngredient(id: $id) {
+                  id
+                }
+              }
+          `;
+      this.apollo.mutate({mutation: query, variables: {id: id}}).subscribe(result => {
+        if (result.errors) {
+          reject(result.errors);
+        } else {
+          resolve(result.data);
+        }
+      });
+    });
+  }
+
+  public updateIngredient(id: String, name: String) {
+    return new Promise((resolve, reject) => {
+      const query = gql`
+              mutation UpdateIngredient($id: String!, $name: String!) {
+                updateIngredient(id: $id, name: $name) {
+                  id
+                }
+              }
+          `;
+      this.apollo.mutate({mutation: query, variables: {id: id, name: name}}).subscribe(result => {
+        if (result.errors) {
+          reject(result.errors);
+        } else {
+          resolve(result.data);
+        }
+      });
+    });
+  }
+
+  public getAllRecipes(): Promise<Recipe[]> {
+    return new Promise((resolve, reject) => {
+      try {
+        const query = gql`
+                    query GetAllRecipes {
+                        getAllRecipes {
+                            name
+                            id
+                        }
+                    }
+                `;
+        this.apollo.watchQuery({
+          query: query
+        }).valueChanges.subscribe(({data, error}) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve((data as any).getAllRecipes);
+          }
+        });
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 }
