2938a6ba9b03450d003ab898df6369d4e48329e0
eliott <eliott.jaquier@gmail.com>
2024-01-03 12:34:12 +0100

    add delete feature on my ingredient page

--- src/app/pages/my-ingredients-page/my-ingredients-page.component.ts
+++ src/app/pages/my-ingredients-page/my-ingredients-page.component.ts
@@ -6,6 +6,7 @@ import {MatBottomSheet} from "@angular/material/bottom-sheet";
 import {
   AddIngredientBottomSheetComponent
 } from "../../fragments/add-ingredient-bottom-sheet/add-ingredient-bottom-sheet.component";
+import {Ingredient} from "../../models/ingredient";
 
 /**
  * The page that allows the user to view and edit their ingredients.
@@ -18,7 +19,7 @@ import {
 export class MyIngredientsPageComponent implements OnInit {
   constructor(private apollo : Apollo, private bottomSheet: MatBottomSheet) { }
 
-  public ingredients:any[] = [];
+  public ingredients:Ingredient[] = [];
 
   async ngOnInit(): Promise<void> {
     let allIngredients = await new CleaverCooksApi(this.apollo).getAllIngredients();
@@ -29,9 +30,17 @@ export class MyIngredientsPageComponent implements OnInit {
     const bottomSheetRef = this.bottomSheet.open(AddIngredientBottomSheetComponent);
     bottomSheetRef.afterDismissed().subscribe((ingredient)=>{
       if(ingredient != undefined){
+        if(this.ingredients.find((i)=>i.id == ingredient.id) != undefined){
+          return;
+        }
         this.ingredients.push(ingredient);
         LocalCookerPreferences.setMyIngredients(this.ingredients);
       }
     });
+  }
+
+  deleteIngredient(ingredient: Ingredient) {
+    this.ingredients = this.ingredients.filter((i)=>i.id != ingredient.id);
+    LocalCookerPreferences.setMyIngredients(this.ingredients);
   }
 }
